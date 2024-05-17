622b3cf0038dc1b7dd8baf05ae399a5921dc08e2
Caio Lopes <caiolopesfv@gmail.com>
2024-05-16 21:50:45 -0300

    enviando atualizações

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
 
 const containers = document.querySelectorAll('.container')
 const header = document.querySelector('.header')
+const footer = document.querySelector('.footer')
 const preloader = document.querySelector('.preloader-container')
 
 function loading() {
     header.style.display = 'none'
+    footer.style.display = 'none'
     containers.forEach(container => {
         container.style.display = 'none'
     });
 
     setTimeout(() => {
         header.style.display = 'block'
+        footer.style.display = 'block'
         containers.forEach(container => {
             container.style.display = 'flex'
         });
